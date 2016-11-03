a524d95029df84a41ea4fb85c0baf40a997ba006
Kezhong Zhang <zhangkz@bupt.edu.cn>
2016-11-04 01:37:14 +0800

    Add the window.

--- manage_data.m
+++ manage_data.m
@@ -1,5 +1,7 @@
-function output=manage_data(input,number_column)
+function [complex_input,output]=manage_data(input,number_column)
+    sps=100;
     complex_input=input*[1;1i];
-    reg_input=reshape(complex_input,number_column,numel(complex_input)*2/number_column);
+    window_input=Windows_receive(complex_input,sps);
+    reg_input=reshape(window_input,number_column/2,numel(window_input)*2/number_column);
     output=kron(real(reg_input),[1;0])+kron(imag(reg_input),[0;1]);
 end
