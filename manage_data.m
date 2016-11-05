ffdebd34e639612e1d3cf81265a0982d11b84114
Kezhong Zhang <zhangkz@bupt.edu.cn>
2016-11-05 10:57:13 +0800

    Little modify.

--- manage_data.m
+++ manage_data.m
@@ -2,7 +2,7 @@ function [reg_input,output]=manage_data(input,number_column)
     sps=100;
     complex_input=input*[1;1i];
     window_input=Windows_receive(complex_input,sps);
-%     window_input=window_input(sps+1:end);
+    window_input=window_input(sps+1:end);
     reg_input=reshape(window_input,number_column/2,numel(window_input)*2/number_column);
     output=kron(real(reg_input),[1;0])+kron(imag(reg_input),[0;1]);
 end
